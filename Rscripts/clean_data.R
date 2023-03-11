780b2cae4efa039b3319e7f4afbd275fc4850e26
AmmarBagharib <ammarbagharib@gmail.com>
2023-03-11 02:54:25 -0800

    rmspe code update

--- Rscripts/clean_data.R
+++ Rscripts/clean_data.R
@@ -1,9 +1,13 @@
 ##  RScript that takes in data set and runs initial cleaning of data
 
-# calculate player wins and mean match stats for winning matches
 player_career <- function(data_set) {
+  
+  #This function calculate player wins and mean match stats for winning matches
+  
   player_wins <- data_set %>%
+    
     group_by(player_id = winner_id) %>%
+    
     summarize(w_height = mean(winner_ht, na.rm =TRUE),
               w_breakpoint_saved_pct = mean(w_bpSaved/w_bpFaced, na.rm =TRUE),
               w_second_serve_win_pct = mean(w_2ndWon / w_svpt,na.rm =TRUE),
@@ -15,11 +19,13 @@ player_career <- function(data_set) {
               w_ace_point_pct = mean(w_ace/w_svpt,na.rm = TRUE)
     ) %>%
     drop_na() %>%
+    
     mutate(player_id = as.character(player_id))
   
   # calculate player losses and mean match stats for losing matches
   player_lose <- data_set %>%
     group_by(player_id = loser_id) %>%
+    
     summarize(l_height = mean(loser_ht, na.rm =TRUE),
               l_breakpoint_saved_pct = mean(l_bpSaved/l_bpFaced, na.rm =TRUE),
               l_second_serve_win_pct = mean(l_2ndWon / l_svpt,na.rm =TRUE),
@@ -30,7 +36,9 @@ player_career <- function(data_set) {
               mean_rank_points_l = mean(loser_rank_points),
               l_ace_point_pct = mean(l_ace/l_svpt,na.rm = TRUE)
     ) %>%
+    
     drop_na() %>%
+    
     mutate(player_id = as.character(player_id))
   
   # join datasets for wins and losses using unique player ids
@@ -47,7 +55,11 @@ player_career <- function(data_set) {
            age = (mean_age_w + mean_age_l) /2,
            mean_rank_points = (mean_rank_points_w + mean_rank_points_l)/2,
            ace_point_pct = (w_ace_point_pct+l_ace_point_pct)/2) %>%
-    select(player_id,height,breakpoint_saved_pct,second_serve_win_pct,first_serve_pct,first_serve_win_pct, win_rate,age,mean_rank_points,ace_point_pct) %>%
+    
+    select(player_id,height,breakpoint_saved_pct,second_serve_win_pct,
+           first_serve_pct,first_serve_win_pct, win_rate,age,mean_rank_points,
+           ace_point_pct) %>%
+    
     drop_na()
   
   return (player_career)
