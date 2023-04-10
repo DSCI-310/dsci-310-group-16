1a7d210b1d112bae25636e0a2baf5a7fd07c7fa0
Miles Brodie <miles.j.brodie@gmail.com>
2023-04-09 17:51:16 -0700

    Included 2_load & 3_clean R scrtipts

---
+++ R/2_load.R
@@ -0,0 +1,11 @@
+library(data.table)
+
+#load file
+atp_data_frame <- data.table::fread(
+  "https://drive.google.com/uc?export=download&id=1fOQ8sy_qMkQiQEAO6uFdRX4tLI8EpSTn"
+)
+
+#remove first unnamed column
+atp_data_frame <- atp_data_frame[, -1]
+
+data.table::fwrite(atp_data_frame,'data/atp2017-2019-1.csv', row.names = FALSE)
