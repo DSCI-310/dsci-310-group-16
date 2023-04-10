ee2b63dd1fcd8bc493873e78343363c7fc240027
Sammy <73415547+syamnadhg@users.noreply.github.com>
2023-04-09 22:57:50 -0700

    Update test-6_regression.R

--- Tests/testthat/test-6_regression.R
+++ Tests/testthat/test-6_regression.R
@@ -1,38 +1,56 @@
-library(testthat)
-library(data.table)
-library(dplyr)
-
-test_file <- here::here('scripts/regression-tables.R')
-
-test_that("Regression tables script produces expected output", {
+test_that("Metric_bind generates correct output for KKNN single regression", {
+  train_df <- data.frame(win_rate = c(0.6, 0.5, 0.4), predictor1 = c(1, 2, 3), predictor2 = c(4, 5, 6))
+  test_df <- data.frame(predictor1 = c(2, 3, 4), predictor2 = c(5, 6, 7))
+  method <- "kknn"
+  metric <- "rmse"
+  target_variable <- "win_rate"
+  model_list <- list("predictor1", "predictor2")
   
-  # generate tables using script
-  source(test_file)
+  output <- metric_bind(train_df = train_df, test_df = test_df, method = method, metric = metric, target_variable = target_variable, model_list = model_list)
   
-  # load expected output
-  expected_kknn_single <- as.data.frame(fread(here::here('tests/testthat/expected/kknn-single-regression.csv')))
-  expected_lm_single <- as.data.frame(fread(here::here('tests/testthat/expected/lm-single-regression.csv')))
-  expected_lm_multiple <- as.data.frame(fread(here::here('tests/testthat/expected/lm-multiple-regression.csv')))
-  expected_kknn_multiple <- as.data.frame(fread(here::here('tests/testthat/expected/kknn-multiple-regression.csv')))
-  expected_all_methods <- as.data.frame(fread(here::here('tests/testthat/expected/all-methods.csv')))
-  expected_best_model <- as.data.frame(fread(here::here('tests/testthat/expected/best-model-prediction.csv')))
+  expected_output <- data.frame(
+    method = "kknn",
+    predictor = c("predictor2", "predictor1"),
+    kmin = c(1L, 1L),
+    metric_value = c(0.14142136, 0.26457513),
+    stringsAsFactors = FALSE
+  )
   
-  # test kknn single regression output
-  expect_identical(kknn_single, expected_kknn_single)
-  
-  # test lm single regression output
-  expect_identical(lm_single, expected_lm_single)
-  
-  # test lm multiple regression output
-  expect_identical(lm_multiple, expected_lm_multiple)
-  
-  # test kknn multiple regression output
-  expect_identical(kknn_multiple, expected_kknn_multiple)
-  
-  # test all methods output
-  expect_identical(all_methods, expected_all_methods)
-  
-  # test best model prediction output
-  expect_identical(final_model, expected_best_model)
-  
+  expect_equal(output, expected_output, check.names = FALSE)
 })
+
+test_that("Metric_bind generates correct output for LM single regression", {
+  train_df <- data.frame(win_rate = c(0.6, 0.5, 0.4), predictor1 = c(1, 2, 3), predictor2 = c(4, 5, 6))
+  test_df <- data.frame(predictor1 = c(2, 3, 4), predictor2 = c(5, 6, 7))
+  method <- "lm"
+  metric <- "rmse"
+  target_variable <- "win_rate"
+  model_list <- list("predictor1", "predictor2")
+  
+  output <- metric_bind(train_df = train_df, test_df = test_df, method = method, metric = metric, target_variable = target_variable, model_list = model_list)
+  
+  expected_output <- data.frame(
+    method = "lm",
+    predictor = c("predictor2", "predictor1"),
+    kmin = NA_integer_,
+    metric_value = c(0.0970797370258968, 0.217944947177033),
+    stringsAsFactors = FALSE
+  )
+  
+  expect_equal(output, expected_output, check.names = FALSE)
+})
+
+test_that("Metric_bind generates correct output for LM multiple regression", {
+  train_df <- data.frame(win_rate = c(0.6, 0.5, 0.4), predictor1 = c(1, 2, 3), predictor2 = c(4, 5, 6), predictor3 = c(7, 8, 9))
+  test_df <- data.frame(predictor1 = c(2, 3, 4), predictor2 = c(5, 6, 7), predictor3 = c(8, 9, 10))
+  method <- "lm"
+  metric <- "rmse"
+  target_variable <- "win_rate"
+  model_list <- list(
+    c("predictor1", "predictor2"),
+    c("predictor1", "predictor3"),
+    c("predictor2", "predictor3"),
+    c("predictor1", "predictor2", "predictor3")
+  )
+  
+  output <- metric_bind(train_df = train_df, test_df = test_df, method = method, metric = metric, target_variable
