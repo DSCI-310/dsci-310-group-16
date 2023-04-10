b881ee9223e7fb3cec2d4f6f0d6181f65d6637dd
Sammy <73415547+syamnadhg@users.noreply.github.com>
2023-04-10 06:31:09 -0700

    Update test-5-08_str-collapse.R

--- Tests/testthat/test-5-08_str-collapse.R
+++ Tests/testthat/test-5-08_str-collapse.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(testthat)
+
+# Test case 1: Check if the function correctly collapses a single string
+test_that("str_collapse correctly collapses a single string", {
+  input <- "my name"
+  expected_output <- "my name"
+  output <- str_collapse(input)
+  expect_equal(output, expected_output)
 })
+
+# Test case 2: Check if the function correctly collapses a vector of strings
+test_that("str_collapse correctly collapses a vector of strings", {
