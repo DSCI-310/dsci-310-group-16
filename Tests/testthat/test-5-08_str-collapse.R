1bd5241431ba7483d2903913ff55f7b9eb3c9a9c
Sammy <73415547+syamnadhg@users.noreply.github.com>
2023-04-10 23:53:49 -0700

    Update test-5-08_str-collapse.R

--- Tests/testthat/test-5-08_str-collapse.R
+++ Tests/testthat/test-5-08_str-collapse.R
@@ -1,20 +1,36 @@
 library(testthat)
 
-Test case 1: All items in str_vector are characters
-test_that("str_collapse returns a string with items of class 'character' separated by '+", {
+Test case 1: str_collapse returns a string with items of class 'character' separated by '+' when all items in 'str_vector' are of class 'character.'
+test_that("str_collapse returns a string with items of class 'character' separated by '+' when all items in 'str_vector' are of class 'character.'", {
 str_vector <- c("my name", "is", "jake")
-expect_equal(str_collapse(str_vector), "my name + is + jake")
+expected_output <- "my name + is + jake"
+expect_equal(str_collapse(str_vector), expected_output)
 })
 
-Test case 2: Some items in str_vector are not characters
-test_that("str_collapse raises an error when not all items in 'str_vector' are of class 'character'", {
+Test case 2: str_collapse raises an error when not all items in 'str_vector' are of class 'character.'
+test_that("str_collapse raises an error when not all items in 'str_vector' are of class 'character.'", {
 str_vector <- c("my name", "is", 2)
 expected_error <- "All items in 'str_vector' should be of class 'character'."
 expect_error(str_collapse(str_vector), expected_error)
 })
 
-Test case 3: str_vector has only one element
+Test case 3: str_collapse returns the single item in 'str_vector' when it has only one element
 test_that("str_collapse returns the single item in 'str_vector' when it has only one element", {
 str_vector <- "my name"
-expect_equal(str_collapse(str_vector), "my name")
+expected_output <- "my name"
+expect_equal(str_collapse(str_vector), expected_output)
+})
+
+Test case 4: str_collapse returns an empty string when passed an empty vector
+test_that("str_collapse returns an empty string when passed an empty vector", {
+str_vector <- character(0)
+expected_output <- ""
+expect_equal(str_collapse(str_vector), expected_output)
+})
+
+Test case 5: str_collapse handles leading/trailing whitespace correctly
+test_that("str_collapse handles leading/trailing whitespace correctly", {
+str_vector <- c(" my name ", " is ", " jake ")
+expected_output <- " my name + is + jake "
+expect_equal(str_collapse(str_vector), expected_output)
 })
