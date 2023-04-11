7be1cc10b324feaeb18d0295c1244b7fe8ab772d
Sammy <73415547+syamnadhg@users.noreply.github.com>
2023-04-10 23:49:00 -0700

    Update test-5-08_str-collapse.R

--- Tests/testthat/test-5-08_str-collapse.R
+++ Tests/testthat/test-5-08_str-collapse.R
@@ -1,12 +1,18 @@
-library(testthat)
-
-# Test case 1: Check if the function correctly collapses a single string
-test_that("str_collapse correctly collapses a single string", {
-  input <- "my name"
-  expected_output <- "my name"
-  output <- str_collapse(input)
-  expect_equal(output, expected_output)
+Test case 1: All items in str_vector are characters
+test_that("str_collapse returns a string with items of class 'character' separated by '+", {
+str_vector <- c("my name", "is", "jake")
+expect_equal(str_collapse(str_vector), "my name + is + jake")
 })
 
-# Test case 2: Check if the function correctly collapses a vector of strings
-test_that("str_collapse correctly collapses a vector of strings", {
+Test case 2: Some items in str_vector are not characters
+test_that("str_collapse raises an error when not all items in 'str_vector' are of class 'character'", {
+str_vector <- c("my name", "is", 2)
+expected_error <- "All items in 'str_vector' should be of class 'character'."
+expect_error(str_collapse(str_vector), expected_error)
+})
+
+Test case 3: str_vector has only one element
+test_that("str_collapse returns the single item in 'str_vector' when it has only one element", {
+str_vector <- "my name"
+expect_equal(str_collapse(str_vector), "my name")
+})
